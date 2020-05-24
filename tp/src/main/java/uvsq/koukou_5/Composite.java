d166d059abcdc6de2931b8025d0cc05e1c729352
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-05-24 21:13:11 +0200

    Create Composite.java

---
+++ tp/src/main/java/uvsq/koukou_5/Composite.java
@@ -0,0 +1,13 @@
+package uvsq.koukou_5;
+
+/**
+ * Interface Composite.
+ * 
+ * @author koussaila
+ */
+public interface Composite {
+	/**
+	 * la methode print La renvoie sous forme de chaine de caractère.
+	 */
+	public void print();
+}
